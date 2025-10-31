f7b8f8fa4074f6927d464e70505729288fc0fc78
Gautham <gauthamskaniyattil@gmail.com>
2025-10-31 14:12:20 +0530

    Added User table

--- app/model/models.py
+++ app/model/models.py
@@ -1,6 +1,6 @@
 from app.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, TIMESTAMP, text
+from sqlalchemy import String, TIMESTAMP, text, ForeignKey
 from datetime import datetime
 
 
@@ -8,6 +8,9 @@ class Anime(Base):
     __tablename__ = "animes"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
     title: Mapped[str] = mapped_column(String, nullable=False)
     genre: Mapped[list[str]] = mapped_column(String, nullable=False)
     total_episodes: Mapped[int] = mapped_column(nullable=False)
@@ -19,5 +22,17 @@ class Anime(Base):
         server_default="0.0", nullable=False
     )  # User rating out of 10
     added_date: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
