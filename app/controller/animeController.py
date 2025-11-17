2ab279cfe2a25e7b25aa60419bb02ea68393f74d
Gautham <gauthamskaniyattil@gmail.com>
2025-11-17 13:03:34 +0530

    Added the controller for updating status

--- app/controller/animeController.py
+++ app/controller/animeController.py
@@ -17,7 +17,6 @@ async def create_anime(new_anime: schema.CreateAnime, db: Session) -> model.Anim
     return anime
 
 
-# TODO  Mark episodes as watched
 async def mark_episode_as_completed(anime_id: int, db: Session):
     anime_to_update = db.query(model.Anime).filter(model.Anime.id == anime_id).first()
     if not anime_to_update:
@@ -29,9 +28,26 @@ async def mark_episode_as_completed(anime_id: int, db: Session):
 
 
 # TODO Change Status of Anime
+async def update_anime_status(id: int, anime_status: str, db: Session):
+    anime_to_update = db.query(model.Anime).filter(model.Anime.id == id).first()
+    if not anime_to_update:
+        return None
+    anime_to_update.status = anime_status
+    db.commit()
+    db.refresh(anime_to_update)
+    return anime_to_update
+
 
 # TODO Rate Anime
+async def rate_an_anime(id: int, rating: float, db: Session):
+    pass
+
 
 # TODO Remove anime
+async def remove_anime(id: int, db: Session):
+    pass
+
 
 # TODO Update anime episodes
+async def update_anime_details(id: int, updated_anime: schema.UpdateAnime, db: Session):
+    pass
